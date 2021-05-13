7e87a4e4d781fac7cc2bf477f7a4043135c67dbb
John Brooks <john.brooks@ookla.com>
2021-05-13 18:03:44 -0500

    move less build from gulp to 11ty

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,8 @@
 var gulp = require('gulp');
-var less = require('gulp-less');
 var del = require('del');
-
-var lessPath = 'src/less/**'
-var lessSrc = 'src/less/ggo.less';
-var lessDest = 'src/less-build';
-
-gulp.task('build', buildLess);
-
-gulp.task('watch', function () {
-  gulp.watch(lessPath, buildLess);
-});
 
 gulp.task('clean', function () {
   return del([
     '_site',
-    'src/less-build',
   ]);
 });
-
-function buildLess (done) {
-  gulp.src(lessSrc)
-    .pipe(less())
-    .pipe(gulp.dest(lessDest))
-  return done();
-}
-
